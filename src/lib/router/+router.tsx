351af1c93b4d9bebe2a5d584f323a11e6e0e93ea
Luke <hello@lukeshafer.com>
2023-01-27 00:18:16 -0500

    Reorganize, add TRPC

--- src/lib/router/+router.tsx
+++ src/lib/router/+router.tsx
@@ -2,37 +2,35 @@ import { Route, Router, Routes, A } from "@solidjs/router";
 import { For, type JSX } from "solid-js";
 
 const routes = import.meta.glob("./_routes/*.tsx", { eager: true }) as Record<
-	string,
-	{ default: () => JSX.Element }
+    string,
+    { default: () => JSX.Element }
 >;
 const routeArray = Object.entries(routes).map(([path, file]) => {
-	const name = path.replace("./_routes/", "").replace(".tsx", "");
-	const component = file.default;
-	return { name, component };
+    const name = path.replace("./_routes/", "").replace(".tsx", "");
+    const component = file.default;
+    return { name, component };
 });
 
-console.log(routeArray);
-
 interface Props {
-	ssrRoute: string;
-	base: string;
+    ssrRoute: string;
+    base: string;
 }
 
 export default function AppRouter({ base, ssrRoute }: Props) {
-	return (
-		<Router base={base} url={ssrRoute}>
-			<main>
-				<nav>
-					<For each={routeArray}>
-						{(route) => <A href={`/${route.name}`}>{route.name}</A>}
-					</For>
-				</nav>
-				<Routes>
-					<For each={routeArray}>
-						{(route) => <Route path={route.name} component={route.component} />}
-					</For>
-				</Routes>
-			</main>
-		</Router>
-	);
+    return (
+        <Router base={base} url={ssrRoute}>
+            <main>
+                <nav>
+                    <For each={routeArray}>
+                        {(route) => <A href={`/${route.name}`}>{route.name}</A>}
+                    </For>
+                </nav>
+                <Routes>
+                    <For each={routeArray}>
+                        {(route) => <Route path={route.name} component={route.component} />}
+                    </For>
+                </Routes>
+            </main>
+        </Router>
+    );
 }

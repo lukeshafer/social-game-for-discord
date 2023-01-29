c774bb4162053863b0ee4c0df84182e763b496c0
Luke <hello@lukeshafer.com>
2023-01-28 22:11:40 -0500

    #15 add prisma and db

--- src/pages/api/auth/[...astroAuth].ts
+++ src/pages/api/auth/[...astroAuth].ts
@@ -1,7 +1,10 @@
 import { AstroAuth, type AstroAuthConfig } from "auth-astro";
 import Discord from "@auth/core/providers/discord";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "@/server/db";
 
 const authOpts: AstroAuthConfig = {
+	adapter: PrismaAdapter(db),
 	providers: [
 		// @ts-ignore
 		Discord({
